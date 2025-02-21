a2f452cb2906f61b4628e5d11ef75ff153c7d918
ajsdklf <gangj277@yonsei.ac.kr>
2025-02-21 15:46:37 +0900

    mobile

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 // app/layout.js
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
